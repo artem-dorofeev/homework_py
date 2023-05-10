16d19b0ab5ace1b84b6281dc5644f4fad6184b2b
Artem Dorofeev <130536391+artem-dorofeev@users.noreply.github.com>
2023-05-10 15:30:02 +0300

    Update main_work02_10.py
    add operetor + - / *

--- main_work02_10.py
+++ main_work02_10.py
@@ -6,3 +6,22 @@ while True:
         break
     except ValueError as e:
         print(f"Not a number, {e}. Try again")
+
+        
+        # a = None
+# b = 10
+# result = None
+# while a == None:
+#     b = input("input operand ")
+# operator = input(">>>")
+# if operator == '+':
+#     result = a + b
+# elif operator == '-':
+#     result = a - b
+# elif operator == '*':
+#     result = a * b
+# elif operator == '/':
+#     result = a / b
+# else:
+#     print("unknow operator")
+# print(result)
