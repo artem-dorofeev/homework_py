7a1283d3ef180014f48d6eeb5e3a4b5cb649b151
Artem Dorofeev <a.dorofeev@ukr.net>
2023-05-21 11:13:50 +0300

    edit hw4-3

--- main_hw4_03.py
+++ main_hw4_03.py
@@ -1,23 +1,34 @@
 def format_ingredients(items):
-    new_items = str(items[0])
+    if items == 0:
+        return items
+    else:
 
-    if len(items) > 1:
-        i = 1
-        while i < len(items):
-            if i == len(items) - 1:
-                new_items += ' and ' + str(items[i])
-            else:
-                new_items += ', ' + str(items[i])
-            i += 1
+        new_items = str(items[0])
 
-    return new_items
+        if len(items) > 1:
+            i = 1
+            while i < len(items):
+                if i == len(items) - 1:
+                    new_items += ' and ' + str(items[i])
+                else:
+                    new_items += ', ' + str(items[i])
+                i += 1
+
+        return new_items
 
 
 pay_list = ["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"]
 
 print(pay_list)
 
+list_2 = [""]
+list_3 = ['10 eggs']
+list_4 = ["2 eggs", "1 liter sugar"]
 
 new_list = format_ingredients(pay_list)
 print(type(new_list))
 print(new_list)
+print(format_ingredients(list_2))
+print(type(format_ingredients(list_2)))
+print(format_ingredients(list_3))
+print(format_ingredients(list_4))
---
+++ main_hw4_03_2.py
@@ -0,0 +1,36 @@
+def format_ingredients(items):
+
+    # new_items = str(items[0])
+
+    if len(items) > 1:
+        new_items = str(items[0])
+        i = 1
+        while i < len(items):
+            if i == len(items) - 1:
+                new_items += ' and ' + str(items[i])
+            else:
+                new_items += ', ' + str(items[i])
+            i += 1
+    elif len(items) == 0:
+        new_items = [""]
+    else:
+        new_items = str(items[0])
+
+    return new_items
+
+
+pay_list = ["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"]
+
+print(pay_list)
+
+list_2 = [""]
+list_3 = ['10 eggs']
+list_4 = ["2 eggs", "1 liter sugar"]
+
+new_list = format_ingredients(pay_list)
+print(type(new_list))
+print(new_list)
+print(format_ingredients(list_2))
+print(type(format_ingredients(list_2)))
+print(format_ingredients(list_3))
+print(format_ingredients(list_4))
