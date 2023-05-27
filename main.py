449e5f01a610843986db4d0dd360e467ff7c4cf2
Artem Dorofeev <130536391+artem-dorofeev@users.noreply.github.com>
2023-05-27 22:17:49 +0300

    change hw4_11

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-ticket = {'quest': 'text question', 'answ1': 'text answ1', 'answ2': 'text'}
+# ticket = {'quest': 'text question', 'answ1': 'text answ1', 'answ2': 'text'}
 
-# grade = {'F': 1, 'FX': 2, 'E': 3, 'D': 3, 'C': 4, 'B': 5, 'A': 5}
-# print(ticket['answ1'])
+# # grade = {'F': 1, 'FX': 2, 'E': 3, 'D': 3, 'C': 4, 'B': 5, 'A': 5}
+# # print(ticket['answ1'])
 
-print(f'Question > {ticket["quest"]}')
+# print(f'Question > {ticket["quest"]}')
 
-for key in ticket:
-    print(key)
+# for key in ticket:
+#     print(key)
 
-for key, value in ticket.items():
-    # print(key, value)
-    print(f'{key} >>> {value}')
+# for key, value in ticket.items():
+#     # print(key, value)
+#     print(f'{key} >>> {value}')
+
+ticket = list(['answ1', 'answ2', 'answ3', 'answ4'])
+print(ticket)
+
+copy_ticket = set(ticket)
+print(copy_ticket)
+print(copy_ticket)
+print(copy_ticket)
+print(copy_ticket)
+print(copy_ticket)
+print(copy_ticket)
+print(copy_ticket)
+print(copy_ticket)
--- main_hw4_11.py
+++ main_hw4_11.py
@@ -30,8 +30,8 @@ def is_valid_password(password):
     key_lower = 0
     key_num = 0
 
-    if len(password) != 8:
-        return False
+    # if len(password) != 8:
+    #     return False
 
     for i in password:
         if ord(i) >= 65 and ord(i) <= 90:
@@ -43,13 +43,13 @@ def is_valid_password(password):
         if ord(i) >= 48 and ord(i) <= 57:
             key_num += 1
 
-    if key_upper == 0 or key_lower == 0 or key_num == 0:
+    if key_upper == 0 or key_lower == 0 or key_num == 0 or len(password) != 8:
         return False
 
     return True
 
 
-new_pass = '1Q2W3E4R'
+new_pass = '!q2w3e4R66'
 
 print(len(new_pass))
 
