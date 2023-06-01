d173a33839e8a8580240d39585fc87ec78d3cf11
Artem Dorofeev <a.dorofeev@ukr.net>
2023-06-01 16:50:34 +0300

    add hw5 10

--- main_hw5_10.py
+++ main_hw5_10.py
@@ -4,16 +4,29 @@ import re
 # def find_word(text, word):
 
 
-text = "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0."
+text = "Guido van Rossum began working on Pytho in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Pytho 0.9.0."
 word = "Python"
 
-for e in text:
-    match_list = re.match(r"\w+", e)
-    if e:
-        print(match_list)
+test = re.search(word, text)
+print(test)
+# print(test.group())
+print(test.span())
+print(test.string)
+
+test2 = test.span()
+print(test2[0])
+
+# for e in text:
+#     match_list = re.match(r"\w+", e)
+#     if e:
+#         print(match_list)
 
 
 # result = re.search(word, text)
 
 # print(result)
 # print(type(result))
+
+# s = "I am 25 years old."
+# age = re.search('\d+', s)
+# print(age.group())
