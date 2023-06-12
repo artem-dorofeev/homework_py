cacd4b3f276a1db42fcceea2ca72d03a493ffcab
Artem Dorofeev <a.dorofeev@ukr.net>
2023-06-12 15:19:45 +0300

    hw06_test

--- main_hw06_test.py
+++ main_hw06_test.py
@@ -1,30 +1,38 @@
 import sys
 from pathlib import Path
-import uuid
+# import uuid
+import shutil
 
 from main_hw06_normalize import normalize
 
 
-CATEGORIES = {"Audio": [".mp3", ".aiff"],
+CATEGORIES = {"Audio": [".mp3", ".aiff", ".m3u"],
               "Documents": [".docx", ".txt", ".pdf", ".doc", ".xlsx", ".xls"],
               "Photo": [".jpg", ".raw", ".nef"],
-              "Video": [".avi", ".mp4", ".mkv"]}
+              "Video": [".avi", ".mp4", ".mkv"],
+              "Archives": [".zip", ".rar", ".tar", ".gz"]}
 
-list_folder = ("Audio", "Documents", "Photo", "Video")
+list_folder = ("Audio", "Documents", "Photo", "Video", "Other", "Archives")
 
 
 def move_file(file: Path, root_dir: Path, categorie: str) -> None:
     target_dir = root_dir.joinpath(categorie)
     if not target_dir.exists():
-        # print(f"Make {target_dir}")
         target_dir.mkdir()
-    # print(path.suffix)
-    # print(target_dir.joinpath(f"{normalize(path.stem)}{path.suffix}"))
     new_name = target_dir.joinpath(f"{normalize(file.stem)}{file.suffix}")
-    if new_name.exists():
-        new_name = new_name.with_name(
-            f"{new_name.stem}-{uuid.uuid4()}{file.suffix}")
-    file.rename(new_name)
+    index = 1
+    while True:
+
+        if new_name.exists():
+            # print(f'name {file} == {new_name}')
+            new_name = new_name.with_name(
+                f"{new_name.stem}_{index}{file.suffix}")
+            index += 1
+        else:
+            file.rename(new_name)
+            break
+
+    # file.rename(new_name)
 
 
 def get_categories(file: Path) -> str:
@@ -35,44 +43,40 @@ def get_categories(file: Path) -> str:
     return "Other"
 
 
-# # def show_all_files(path: Path) -> None:
-# #     data = []
-# #     for item in path.iterdir():
-# #         if item.is_dir():
-# #             data.extend(show_all_files(item))
-# #         else:
-# #             data.append(item)
-# #     return data
-
-# def show_all_files(path: Path) -> None:
-#     for item in path.iterdir():
-#         if item.is_dir():
-#             show_all_files(item)
-    #     continue
-    # print(item)
-
-
 def sort_folder(path: Path) -> None:
 
     for item in path.glob("**/*"):
         # print(item)
 
         if item.is_file():
-            print(f'this is file {item}')
-            # cat = get_categories(item)
-            # move_file(item, path, cat)
+            # print(f'this is file {item}')
+            cat = get_categories(item)
+            move_file(item, path, cat)
 
-    for item in path.glob("**/*"):
-        if item.is_dir():
-            # print(f'this is dir {item}')
-            # print(item.name)
 
-            if item.name not in list_folder:
+def del_emppty_folders(path: Path) -> None:
+    for item in path.iterdir():
+        if item.name not in list_folder:
 
+            if item.is_dir():
+                del_emppty_folders(item)
                 try:
                     item.rmdir()
                 except OSError:
+                    print(f'folder {item.name} is not emppty')
                     continue
+
+            continue
+
+
+def upack_archive(path: Path) -> None:
+    arch_path = path.joinpath("Archives")
+    for item in arch_path.iterdir():
+        # print(f'archive - {item.stem}')
+        output_arch = arch_path.joinpath(item.stem)
+        # print(output_arch)
+        output_arch.mkdir()
+        shutil.unpack_archive(item, output_arch)
 
 
 def main():
@@ -85,8 +89,8 @@ def main():
         return f"Folder with path {path} dos`n exists."
 
     sort_folder(path)
-    # delete_emppty_folders(path)
-    # upack_archive(path)
+    del_emppty_folders(path)
+    upack_archive(path)
 
     return "All ok"
 
