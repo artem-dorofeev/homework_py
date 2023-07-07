fd6e15dc0687d72364ee7ca494fa969ef7d93309
Artem Dorofeev <a.dorofeev@ukr.net>
2023-07-07 21:44:46 +0300

    hw10_14

--- main_hw10_14.py
+++ main_hw10_14.py
@@ -5,12 +5,33 @@ class Contacts:
         self.contacts = []
 
     def list_contacts(self):
-        pass
+        return self.contacts
 
     def add_contacts(self, name, phone, email, favorite):
-        pass
+        self.contacts.append({"id": self.current_id,
+                              "name": name,
+                              "phone": phone,
+                              "email": email,
+                              "favorite": favorite})
+        self.current_id += 1
+
+        return Contacts.list_contacts(self)
+
+cont = Contacts()
+cont.add_contacts("Artem", "1241141", "a.e@ukr.net", True)
+cont.add_contacts("Artem", "1241141", "a.e@ukr.net", True)
+cont2 = Contacts()
+cont2.add_contacts("Artem", "1241141", "a.e@ukr.net", True)
+
+cont2.add_contacts('Wylie Pope', '(692) 802-2949', 'est@utquamvel.net', False)
+
+cont2.add_contacts('Cyrus Jackson', '(501) 472-5218', 'nibh@semsempererat.com', False)
+
+print(cont.list_contacts())
+print(cont2.list_contacts())
 
 
+# print(result)
 """
 
 Реалізуйте клас Contacts, який працюватиме з контактами. На першому етапі ми додамо два методи.
